5a1de1dbbb5df9f97e8ca66da7cfb9cf2c9bbf43
Esteban <estebanvelaren@gmail.com>
2022-04-10 21:06:31 -0500

    first commit

---
+++ README.txt
@@ -0,0 +1,26 @@
+Instructions:
+
+1. Make sure to use an environment with python 3.9.11
+
+You can easily do so by installing Anaconda:
+	https://docs.anaconda.com/anaconda/install/windows/
+Then you can open a terminal and create the environment via:
+	conda create -n py39 python=3.9
+	conda activate py39
+Feel free to use Visual Code or Pycharm to edit the python file.
+
+Note: If you have python recently installed it is most likely to be 3.9,
+if so you can skip the previous step and use your environment on visual code or pycharm
+
+2. Requirements: Install the following dependencies:
++ pandas
++ geopandas
++ bokeh
+Note: 	You can do by typing in the terminal: pip install pandas (for example)
+	Maybe you are required to install something else, if so the
+	console will tell you what and you just need to do pip install...
+
+3. From the terminal you can mount the bokeh server using the following command:
+bokeh serve --show main.py
+
+4. At this point a new tab in your browser is created with the dashboard created in the python file.
---
+++ main.py
@@ -0,0 +1,111 @@
+# Required modules
+import pickle
+import pandas as pd
+import numpy as np
+import geopandas as gdp
+import json
+from bokeh.io import curdoc
+from bokeh.plotting import figure
+from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, Slider, HoverTool, ColumnDataSource
+from bokeh.palettes import brewer
+from bokeh.layouts import widgetbox, row, column
+
+# Loading pickles: 
+# TODO: Add the rest of datasets. 
+# Optional: We can either obtain the datasets in another python file and load them here via pickle !OR! put everything in this python file and not use pickles.
+with open('df_gas.pickle', 'rb') as handle:
+    df_gas = pickle.load(handle)
+with open('gdf.pickle', 'rb') as handle:
+    gdf = pickle.load(handle)
+
+# Function to match the 2 tables at the year selected in the slider
+def json_data_selector(selectedYear):
+    yr = selectedYear
+    df_yr = df_gas[df_gas['Year'] == yr]
+    merged = gdf.merge(df_yr, on='Country')
+    #merged.fillna('No data', inplace = True)
+    merged_json = json.loads(merged.to_json())
+    json_data = json.dumps(merged_json)
+    return json_data
+
+#Input GeoJSON source that contains features for plotting.
+geosource = GeoJSONDataSource(geojson = json_data_selector(2011))
+#Define a sequential multi-hue color palette.
+palette = brewer['Greens'][8]
+#Reverse color order so that dark blue is highest obesity.
+palette = palette[::-1]
+#Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors. Input nan_color.
+# TODO Change High Range
+color_mapper = LinearColorMapper(palette = palette, low = 0.00, high = 30000.000, nan_color = '#d9d9d9')
+#Define custom tick labels for color bar.
+tick_labels = {'5000': '5000 m',}
+#Add hover tool
+hover = HoverTool(tooltips = [('Country Code: ','@Country'),
+                              ('Russian Natural Gas Import','@Gas_Import')])
+#Create color bar.
+color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8,width = 20, height = 500,
+                     border_line_color=None,location = (0,0), orientation = 'vertical', major_label_overrides = tick_labels)
+#Create figure object.
+p = figure(title = 'Russian export influence over Europe: Natural Gas', plot_height = 550 , plot_width = 650, toolbar_location = None, tools = [hover])
+p.xaxis.visible = False
+p.yaxis.visible = False
+p.xgrid.grid_line_color = None
+p.ygrid.grid_line_color = None
+
+#Add patch renderer to figure (for the map)
+p.patches('xs','ys', source = geosource, fill_color = {'field' :'Gas_Import', 'transform' : color_mapper},
+          line_color = 'black', line_width = 0.5, fill_alpha = 1)
+#Specify layout
+p.add_layout(color_bar, 'right')
+p.add_tools(hover)
+
+def update_plot(attr, old, new):
+    yr = slider.value
+    new_data = json_data_selector(yr)
+    geosource.geojson = new_data
+    p.title.text = 'Russian export influence over Europe: Natural Gas, %d' %yr
+
+# Make a slider object: slider 
+slider = Slider(title = 'Year',start = 2011, end = 2020, step = 1, value = 2011)
+# callback.args['']
+slider.on_change('value', update_plot)
+
+# Make a column layout of widgetbox(slider) and plot, and add it to the current document
+l = column(p,widgetbox(slider))
+
+#--------------------------------------- SCATTER PLOT (TODO Replace by one of our graphs)
+import random
+count = 10
+x = range(count)
+y = random.sample(range(0, 101), count)
+p2 = figure(plot_width=400, plot_height=400)    # figure is a type of plot
+# using various glyph methods to create scatter
+# plots of different marker shapes
+p2.circle  (x, y, size=30, color='red', legend_label='circle')
+p2.line    (x, y, width=2, color='blue', legend_label='line')
+p2.triangle(x, y, size=10, color='gold', legend_label='triangle')
+
+l2 = row(l,p2)
+#--------------------------------------- BAR CHART (TODO Replace by one of our graphs)
+# create a Pandas dataframe
+df = pd.DataFrame(dict(
+    x = [1, 2, 3, 4, 5],
+    y = random.sample(range(1,11), 5), 
+))
+# create a ColumnDataSource obj using a dataframe
+src = ColumnDataSource(data=df)
+p3 = figure(plot_width=400, plot_height=400)
+p3.vbar(x = 'x',
+    top = 'y',
+    source = src,
+    width = 0.5,
+    bottom = 0,
+    color = 'lightgreen')
+
+l3 = column(l2,p3)
+#------------------------------------------
+curdoc().add_root(l3)
+
+#Display plot inline in Jupyter notebook
+# output_notebook()
+# =show(l)
